e5f874861c59528293ac53e7b8f0a013cebc5a6e
Shyju Viswambaran <shyjuezy@gmail.com>
2024-03-06 21:21:54 -0600

    Added ddb

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 result=$(aws sts get-caller-identity --output text --query 'Account') && export AWS_ACCOUNT=$result || { echo "could not get account ID"; exit 1; }
+pip install -r requirements.txt --upgrade --use-feature=fast-deps
 
 echo "===> Building src"
 
@@ -8,7 +9,6 @@ echo "===> Building src"
 SERVICE_NAME=${PWD##*/}
 REGIONS=(
   "us-east-1"
-  # "us-west-2"
 )
 
 echo $AWS_ACCOUNT
@@ -27,11 +27,23 @@ for REGION in ${REGIONS[@]}; do
     --output-template-file $OUTPUT_FILE
 
 
-  sam deploy \
-    --no-fail-on-empty-changeset \
-    --template-file $OUTPUT_FILE \
-    --stack-name $SERVICE_NAME \
-    --region $REGION \
-    --s3-bucket coxauto-rpp-$REGION-$AWS_ACCOUNT-temp \
-    --capabilities CAPABILITY_NAMED_IAM
+  if [ -z "$1" ]
+  then
+    sam deploy \
+      --no-fail-on-empty-changeset \
+      --template-file $OUTPUT_FILE \
+      --stack-name $SERVICE_NAME \
+      --region $REGION \
+      --s3-bucket coxauto-rpp-$REGION-$AWS_ACCOUNT-temp \
+      --capabilities CAPABILITY_NAMED_IAM
+  else
+    sam deploy \
+      --no-fail-on-empty-changeset \
+      --template-file $OUTPUT_FILE \
+      --stack-name $1-$SERVICE_NAME \
+      --region $REGION \
+      --s3-bucket coxauto-rpp-$REGION-$AWS_ACCOUNT-temp \
+      --capabilities CAPABILITY_NAMED_IAM \
+      --parameter-overrides AliasName="${1}"
+  fi
 done
--- src/lambda.py
+++ src/lambda.py
@@ -1,15 +1,33 @@
 import json
-import time
+import boto3
+import logging
 
-print('Loading function')
+from datetime import datetime
+import os
+
+print("Loading function")
+table_name = os.environ.get("TABLE_NAME")
+dynamodb_client = boto3.resource("dynamodb")
+table = dynamodb_client.Table(table_name)
 
 
 def handler(event, context):
-    # print("Received event: " + json.dumps(event, indent=2))
-    time.sleep(5)
+    logging.info({"event": json.dumps(event)})
 
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    # raise Exception('Something went wrong')
+    if "name" in event:
+        name = event["name"]
+        logging.info("Hello " + name)
+        add_data_to_dynamodb(event)
+
+        return name  # Echo back the first key value
+    else:
+        logging.error("name not found in event")
+        raise Exception("name not found in event")
+
+
+def add_data_to_dynamodb(event):
+    event["timestamp"] = str(datetime.now())
+    response = table.put_item(Item=event)
+    logging.info("PutItem succeeded:")
+    logging.info({"response": json.dumps(response, indent=4)})
+    return response
